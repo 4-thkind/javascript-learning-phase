4573cbc42b4597bada9b6877f20aaa258b45c8ba
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-01-04 18:55:41 +0000

    just array's 3 element taken out

--- basics/functionuse.js
+++ basics/functionuse.js
@@ -6,3 +6,12 @@ function message(anyobject){
     return `${anyobject.name} has added item worth ${anyobject.price}`
 }
 console.log(message(userinfo))
+
+const myArray =[1,2,3,4,5,6,7];
+
+function giveu(arrayhere){
+    console.log("the 3rd element of array entered is :")
+    return arrayhere[2];
+}
+let reference = giveu(myArray)
+console.log(reference)

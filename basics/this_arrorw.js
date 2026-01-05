101002128b5fb57f6371e9b65fb420b48222fb4f
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-01-05 10:39:08 +0000

    IIFE use named and unnamed

--- basics/this_arrorw.js
+++ basics/this_arrorw.js
@@ -5,3 +5,16 @@ const anything = ()=> {
 }
 
 anything()
+
+const two = function(){
+    console.log(this);
+}
+two();
+
+(function(){
+    console.log("helloe world!");
+
+})();
+(()=>{
+    console.log("hello world from arrow fuction")
+})()

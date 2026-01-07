962f6f6d742666991983fc5c5b16765e5aaf8f4f
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-01-07 14:50:29 +0000

    corrected file

--- basics/this_arrorw.js
+++ basics/this_arrorw.js
@@ -18,13 +18,12 @@ two();
 (()=>{
     console.log("hello world from arrow fuction")
 })()
-
 const arr=[1,2,3,4,5,67];
 
 arr.forEach(function (item,value,arr){
     console.log(item,value,arr);
 });
-const arr=[1,2,3,4,5,67];
+
 const newarr = arr.map((num)=>(num*10))
                 .map((num)=>(num+2))
                 .filter((n)=>n>12);

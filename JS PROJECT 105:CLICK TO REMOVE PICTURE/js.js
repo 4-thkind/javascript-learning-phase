7d050bc0fb169d232140ea81e6c83d718d746c11
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-04-16 13:10:40 +0000

    indentation edit

--- JS PROJECT 105:CLICK TO REMOVE PICTURE/js.js
+++ JS PROJECT 105:CLICK TO REMOVE PICTURE/js.js
@@ -1,6 +1,6 @@
 document.querySelector('#id').addEventListener("click",
     function(e){
-        console.log(e.target.parentNode);
+       console.log(e.target.parentNode);
        if(e.target.tagName === 'IMG'){
         let removeIT=e.target
         removeIT.remove();
